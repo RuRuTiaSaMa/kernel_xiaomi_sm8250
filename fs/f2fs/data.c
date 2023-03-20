2bc8be972d95cb2b309d4e8a3b9728bf1ad2aed5
Yangtao Li <frank.li@vivo.com>
2023-03-21 01:31:36 +0800

    f2fs: remove else in f2fs_write_cache_pages()
    As Christoph Hellwig point out:
    
    	Please avoid the else by doing the goto in the branch.
    
    Signed-off-by: Yangtao Li <frank.li@vivo.com>
    Reviewed-by: Chao Yu <chao@kernel.org>
    Signed-off-by: Jaegeuk Kim <jaegeuk@kernel.org>
    Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

--- fs/f2fs/data.c
+++ fs/f2fs/data.c
@@ -3226,11 +3226,8 @@ continue_unlock:
 			}
 
 			if (PageWriteback(page)) {
-				if (wbc->sync_mode != WB_SYNC_NONE)
-					f2fs_wait_on_page_writeback(page,
-							DATA, true, true);
-				else
 					goto continue_unlock;
+				f2fs_wait_on_page_writeback(page, DATA, true, true);
 			}
 
 			if (!clear_page_dirty_for_io(page))
